19f0e6337b8eb085df6776d9ce9106a667a00607
Szilágyi Ádám <sz.adam1989@gmail.com>
2025-07-26 14:42:28 +0200

    Update settings.py

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'Django/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'Django/static')
     ]
 
 # Default primary key field type
