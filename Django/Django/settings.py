8c7d522c54362746cd1356778421466f8379e7d0
Szilágyi Ádám <sz.adam1989@gmail.com>
2025-07-26 14:38:44 +0200

    Update settings.py

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Django/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
