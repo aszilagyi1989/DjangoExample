8ceef2e8c61754c29457b34417cfbbd8cdcbb3af
vl05953 <adam.szilagyi@ksh.hu>
2025-08-07 13:18:12 +0200

    Adding new improvements

--- Django/Django/views.py
+++ Django/Django/views.py
@@ -1,10 +1,7 @@
-""" from django.http import HttpResponse """
 from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home.html')
-"""     return HttpResponse("Hello World! I'm Home.") """
 
 def about(request):
     return render(request, 'about.html')
-"""     return HttpResponse("My About page.") """
